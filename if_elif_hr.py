429be7a71fd3967ec4696d15d1b5afb2ec5f50b9
ChanduReddy999 <113517229+ChanduReddy999@users.noreply.github.com>
2022-09-28 23:10:07 +0530

    Add files via upload

---
+++ if_elif_hr.py
@@ -0,0 +1,49 @@
+"""
+if elif problem in python programming
+"""
+
+# print("Hello, World!")
+
+# method 1
+
+# if __name__ == '__main__':
+#     n = int(input().strip())
+
+#     if n%2 != 0:
+#         print("Weird")
+#     else:
+#         if n>=2 and n<=5:
+#             print("Not Weird")
+#         elif n>=6 and n<=20:
+#             print("Weird")
+#         else:
+#             print("Not Weird")
+
+
+# method 2
+
+# n = int(input())
+
+# if n%2 == 1:
+#    print('Weird')
+# elif n>=2 and n<=5:
+#     print('Not Weird')
+# elif n>=6 and n<=20:
+#     print('Weird')
+# elif n>20: 
+#     print('Not Weird')
+
+
+# method 3
+
+# val = int(input())
+# output = "Weird"
+# if val%2 == 1:
+#     pass
+# elif 2 <= val < 5:
+#     output = "Not Weird"
+# elif 6 <= val <= 20:
+#     pass
+# else:
+#     output = "Not Weird"
+# print(output)
